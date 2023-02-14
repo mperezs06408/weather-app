d708581ca59b489ccfe5c3c98083eb9bf6e4b83c
mperezs06408 <mperezs06408@universidadean.edu.co>
2023-02-13 22:21:56 -0500

    Fix (api) Clear API cache

--- src/api/openWeatheAPI.js
+++ src/api/openWeatheAPI.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const WEATHER_URL = 'https://api.openweathermap.org/data/2.5';
-const WEATHER_KEY = '218249339ae75426930a0e95e157057b';
+const WEATHER_KEY = '';
 
 const weatherInstance = axios.create({
     baseURL: WEATHER_URL
